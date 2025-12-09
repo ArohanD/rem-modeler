0ef9255fc5b969b3a4f42f557664ab2042ef7372
Arohan Dutt <arohandutt@gmail.com>
2025-12-09 00:04:34 -0500

    Separate geoprocessing

--- src/interfaces/__init__.py
+++ src/interfaces/__init__.py
@@ -3,6 +3,7 @@ from .interfaces import (
     minmax_widget,
     hillshade_widget,
     centerline_widget,
+    manual_centerline_widget,
     interactive_min_max,
     interactive_hillshade,
     derive_centerline,
@@ -13,11 +14,23 @@ from .interfaces import (
     ViewerState
 )
 
+from .geoprocessing import (
+    skeleton_to_linestring,
+    extract_centerline,
+    compute_hillshade,
+    query_osm_waterways,
+    build_linestrings_from_osm,
+    densify_line,
+    snap_centerline_to_channel,
+)
+
 __all__ = [
+    # Interface functions
     "interactive_raster_viewer",
     "minmax_widget",
     "hillshade_widget",
     "centerline_widget",
+    "manual_centerline_widget",
     "interactive_min_max",
     "interactive_hillshade",
     "derive_centerline",
@@ -25,5 +38,13 @@ __all__ = [
     "interactive_osm_centerline",
     "display_centerline_overlay",
     "display_raster",
-    "ViewerState"
+    "ViewerState",
+    # Geoprocessing functions
+    "skeleton_to_linestring",
+    "extract_centerline",
+    "compute_hillshade",
+    "query_osm_waterways",
+    "build_linestrings_from_osm",
+    "densify_line",
+    "snap_centerline_to_channel",
 ]
